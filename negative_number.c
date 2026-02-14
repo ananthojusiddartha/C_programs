2da17798023dba3fac107037a8bd9d043a2defbb
ananthojusiddartha <ananthojusiddartha@gmail.com>
2026-02-14 09:18:19 +0530

    Add files via upload

---
+++ negative_number.c
@@ -0,0 +1,12 @@
+//Program to check the given number is negative number
+#include<stdio.h>
+int main()
+{
+int  a;
+printf("Enter the Number \n");
+scanf("%d",&a);
+int res;
+res=(a<0);
+printf("The %d is negative number is %d \n",a,res);
+    return 0;
+}
