7c782367f1c58e65b54b653813b0dc381df23e85
ananthojusiddartha <ananthojusiddartha@gmail.com>
2026-02-14 09:17:38 +0530

    Add files via upload

---
+++ positive_number.c
@@ -0,0 +1,12 @@
+//Program to check the given number is positive number
+#include<stdio.h>
+int main()
+{
+int  a;
+printf("Enter the Number \n");
+scanf("%d",&a);
+int res;
+res=(a>0);
+printf("The %d is positive number is %d \n",a,res);
+    return 0;
+}
